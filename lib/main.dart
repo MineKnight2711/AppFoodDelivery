919734373b79abdaf7ff86d812690f5b33c68bfc
MineKnight2711 <witherdragonlair@gmail.com>
2023-05-13 12:47:30 +0700

    giỏ hàng,thanh toán

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:app_food_2023/screens/login_register/login_screen.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
